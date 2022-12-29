6f2abb836850c2ec417d372156b9642298259b46
jsta <stachel2@msu.edu>
2022-12-29 13:44:05 -0700

    cleanup

--- 2022/1/app/code.f90
+++ 2022/1/app/code.f90
@@ -1,28 +1,25 @@
+module procedures
+   implicit none
+
+contains
+
+end module procedures
+
 program example_loadtxt
    use stdlib_io, only: loadtxt
    implicit none
 
-   ! INTEGER, PARAMETER                :: M = 10, N = 2000
-   ! REAL, DIMENSION(0:N-1)            :: vector
-   ! LOGICAL, DIMENSION(M)             :: idx
-
-   real, allocatable  :: x(:, :)
-   real, dimension(0:10)  :: x_sub
-   integer            :: j
-   integer :: t
-   integer, dimension(1)             :: j_max
+   real, allocatable       :: x(:, :)
+   real, dimension(0:10)   :: x_sub
+   integer                 :: j
+   integer, dimension(1)   :: j_max
    integer, dimension(0:1) :: gap_locs
-   integer, dimension(1) :: gap_loc
+   integer, dimension(1)   :: gap_loc
 
    call loadtxt('input_small', x)
    !    print *, x
    !    print *, sum(x)
    ! print *, x(2,:)
-
-   ! vector(:) = 0
-   ! idx(1:) = (/ (x(j,:) == 0, j=1,10)/)
-   ! print *, idx
-   ! print *, size(vector)
 
    ! get gap location
    gap_locs = 0
